666c76f3c0880398b3a6213ad14896cbb287f754
Konstantine Mushegian <kmushegi@bowdoin.edu>
2016-11-06 20:42:54 -0500

    initial code thru min-conflicts

--- nqueens/queens.cpp
+++ nqueens/queens.cpp
@@ -4,9 +4,9 @@
 
 typedef std::vector<std::vector<int>> board2D;
 
-board2D chessBoard;
+#define N 8
 
-board2D generateBoard(int n) {
+board2D initializeBoard(int n) {
 	board2D chessBoard;
 	for(int i=0; i<n; i++) {
 		std::vector<int> row = {0};
@@ -26,8 +26,87 @@ void printBoard(board2D b) {
 	std::cout<<std::endl;
 }
 
+int numberOfConflicts(board2D &b, int r, int c) {
+	int numConflicts = 0;
+	// for(int i=0; i<N; i++) {
+	// 	if(i == c) {
+	// 		continue;
+	// 	}
+	// 	if(b[i] == r || abs(i-c) == abs(b[i] - r)) {
+	// 		numConflicts++;
+	// 	}
+	// }
+	// return numConflicts;
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			if(((i == r) || (j == c) || (abs(i-r) == abs(j-c))) && b[i][j] == 1) {
+				numConflicts++;
+			}
+		}
+	}
+	return numConflicts;
+}
+
+//if there is a conflict return queen row, else return -1
+std::vector<int> isSolution(board2D &b, std::vector<int> queenLocations) {
+	std::vector<int> conflictingVariables;
+
+	for(int r = 0; r < N; r++) {
+		int c = queenLocations[r];
+		int conflicts = numberOfConflicts(b,r,c);
+		if(conflicts != 0) {
+			conflictingVariables.push_back(r);
+		}
+	}
+	return conflictingVariables;
+}
+
+board2D minConflicts(board2D b, std::vector<int> queenLocations, int maxSteps = 500) {
+	
+	board2D current = b;
+	for(int i=0; i<maxSteps; i++) {
+
+		std::vector<int> conflictingVars = isSolution(current, queenLocations);
+		if(conflictingVars.size() == 0) {
+			return current;
+		} else {
+			int randomConflictingVar = rand() % conflictingVars.size();
+			int currentMin = numberOfConflicts(b,randomConflictingVar,queenLocations[randomConflictingVar]); //was INT_MAX
+
+			//TODO: randomly choose between tied queen positions
+			for(int j=0; j<N; j++) {
+				int t = numberOfConflicts(b,randomConflictingVar,j);
+				if(t < currentMin) {
+					currentMin = t;
+				}
+			}
+			current[randomConflictingVar][queenLocations[randomConflictingVar]] = 0;
+			queenLocations[randomConflictingVar] = currentMin;
+			current[randomConflictingVar][currentMin] = 1;
+			printBoard(current);
+		}
+	}
+	return current;
+}
+
+std::vector<int> placeQueensRandom(board2D &b, int n) {
+	std::vector<int> randomNumbers;
+
+	for(int i=0; i<n; i++) {
+		int r = rand()%n;
+		b[i][r] = 1;
+		randomNumbers.push_back(r);
+	}
+	return randomNumbers;
+}
+
+
 int main() {
-	board2D b = generateBoard(10);
+	srand(time(NULL));
+	board2D b = initializeBoard(N);
+	std::vector<int> initialSeed = placeQueensRandom(b,N);
 	printBoard(b);
+	board2D c = minConflicts(b,initialSeed,10);
+	printBoard(c);
 	return 0;
 }
