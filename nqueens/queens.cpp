aceb3e4121581a2df32f5050a28815c86d7a4286
chunnewell <clarahunnewell@dhcp-212-50.wireless.bowdoin.edu>
2016-11-09 20:39:12 -0500

    greedy working

--- nqueens/queens.cpp
+++ nqueens/queens.cpp
@@ -5,8 +5,8 @@
 //using namespace std;
 typedef std::vector<std::vector<int>> board2D;
 
-#define N 4
-#define STEPS 1
+#define N 8
+#define STEPS 500
 #define GREEDY 1
 
 
@@ -89,55 +89,77 @@ board2D minConflictsGreedy(board2D b, std::vector<int> queenLocations, int maxSt
 			return current;
 		} else {
 			std::vector<Info> currentMins;
-
 			Info info;
 			info.conflicts = INT_MAX;
 			info.row = -1;
 			info.col = -1;
-			std::cout << "hello" << std::endl;
-			for(int r=0; r < conflictingVars.size(); r++) {
+			//currentMins.push_back(info);
+			//std::cout << "LOOK HERE" << currentMins[0].row << std::endl;
+			/*std::cout << "hello these are the conflicting vars" << std::endl;
+			for (int q = 0; q<conflictingVars.size(); q++) {
+				std::cout<< conflictingVars[q] << " " ;
+			}
+			std::cout << std::endl << "hello these are the queen locations" << std::endl;
+			for (int q = 0; q<queenLocations.size(); q++) {
+				std::cout<< queenLocations[q] << " " << std::endl;
+			}*/
 
+			for(int m=0; m < conflictingVars.size(); m++) {
+				int r = conflictingVars[m];
 				current[r][queenLocations[r]]=0; // set current spot to 0
+				//std::cout<<"currently working on " <<std::endl;
+				//printBoard(current);
+				//std::cout<< std::endl;
 
 				for(int c = 0; c < queenLocations.size(); c++) {
 					// set current spot to 0
-					current[r][c]=0;
-					std::cout << "testing " << r << " " << c <<std::endl;
+					//current[r][c]=0;
+					//std::cout << "testing " << r << " " << c <<std::endl;
 					int tmp = numberOfConflicts(current,r,c);
 
 					if(tmp < info.conflicts) {
-						std::cout << "updating current min to " << tmp <<std::endl;
-						std::cout << "updating current queen to " << r  << " and column " << c<<std::endl;
+						//std::cout << "updating current min to " << tmp <<std::endl;
+						//std::cout << "updating current queen to " << r  << " and column " << c<<std::endl;
 						info.conflicts = tmp;
 						info.row = r;
 						info.col = c;
 					if (currentMins.empty() == false){
-						 std::cout<<"clearing"<<std::endl;
+						 //std::cout<<"clearing"<<std::endl;
 						currentMins.clear();
 					}
 					currentMins.push_back(info);
 
 					}
 					else if (tmp == info.conflicts){
-						std::cout << "new min found" << std::endl;
+						//std::cout << "new min found" << std::endl;
+						info.row = r;
+						info.col = c;
 						currentMins.push_back(info);
 					}
+
 				}
 				current[r][queenLocations[r]] = 1; // return the queen to its current location
 			}
+			/*std::cout << std::endl << "hello these are the possibilities" << std::endl;
+			for (int q = 0; q<currentMins.size(); q++) {
+				std::cout<< currentMins[q].row << " and " << currentMins[q].col << " " << std::endl;
+			}*/
 			int tieBreaker = rand() % currentMins.size();
 			int queenToMove = currentMins[tieBreaker].row;
 			current[queenToMove][queenLocations[queenToMove]] = 0; 
-			std::cout << "move to make " << currentMins[tieBreaker].row <<std::endl;
-			printBoard(current);
-			std::cout << std::endl;
+			//std::cout << "move to make " << currentMins[tieBreaker].row << " " << currentMins[tieBreaker].col <<std::endl;
+			//printBoard(current);
+			//std::cout << std::endl;
 			queenLocations[queenToMove] = currentMins[tieBreaker].col;
 			current[queenToMove][currentMins[tieBreaker].col] = 1;
+			//std::cout<< "move made" << std::endl;
 			//printBoard(current);
 		}
 	}
 	return current;
 }
+
+
 
 board2D minConflictsRandom(board2D b, std::vector<int> queenLocations, int maxSteps) {
 	std::cout << "MinConflicts. STEPS = "<< maxSteps << std::endl;
@@ -216,12 +238,21 @@ board2D randomOrMinConflicts(board2D b, std::vector<int> queenLocations, int max
 
 std::vector<int> placeQueensRandom(board2D &b, int n) {
 	std::vector<int> randomNumbers;
-
+	
 	for(int i=0; i<n; i++) {
 		int r = rand()%n;
 		b[i][r] = 1;
 		randomNumbers.push_back(r);
 	}
+		/*
+	b[0][0] = 1;
+	b[1][3] = 1;
+	b[2][3] = 1;
+	b[3][2] = 1;
+	randomNumbers.push_back(0);
+	randomNumbers.push_back(3);
+	randomNumbers.push_back(3);
+	randomNumbers.push_back(2);*/
 	return randomNumbers;
 }
 
